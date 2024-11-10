890c7739619c96dc3f4d0376fbd13e1713a8a26e
Emircan Maden <emircanmaden123@gmail.com>
2024-11-10 21:26:54 +0300

    final

--- cikis.js
+++ cikis.js
@@ -1,7 +1,7 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Çıkış yapıldığında
+function cikisYap() {
+    // Çıkış yapıldığında sessionStorage'dan oturum bilgisini sil
     sessionStorage.removeItem("giris_yapildi");
 
     // Çıkış sonrası giris.html sayfasına yönlendir
     window.location.href = "giris.html";
-});
+}
